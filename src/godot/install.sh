d7e9bbf63559c123e435425dadcd718c3a2084c3
Yuya Watanabe <nise.nabe@gmail.com>
2024-03-02 12:19:13 +0000

    install godot

--- src/godot/install.sh
+++ src/godot/install.sh
@@ -2,3 +2,12 @@
 
 set -e
 
+. ./library_scripts.sh
+
+ensure_nanolayer nanolayer_location "v0.5.6"
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.25" \
+    --option repo='godotengine/godot' --option binaryNames='godot' --option assetRegex="stable_linux"
--- test/godot/test.sh
+++ test/godot/test.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "execute command" bash -c "godot"
+check "execute command" bash -c "godot -h"
 
 reportResults
