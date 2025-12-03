1ed8eff80903e7d6a71e4cb4e7f38e25cf2419c8
nise_nabe <nise.nabe@gmail.com>
2025-12-04 08:29:45 +0900

    update gh-release to v1.0.26

--- src/godot/install.sh
+++ src/godot/install.sh
@@ -9,5 +9,5 @@ ensure_nanolayer nanolayer_location "v0.5.6"
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.25" \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.26" \
     --option repo='godotengine/godot' --option binaryNames='godot' --option assetRegex="stable_linux"
